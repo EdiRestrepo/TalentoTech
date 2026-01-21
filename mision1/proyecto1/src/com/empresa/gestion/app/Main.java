c63b986d8dc6eeba7f63939a8f028d308487c7b5
EdiRestrepo <edisonestival@gmail.com>
2026-01-20 20:04:20 -0500

    Add Main and NominaService classes for employee management system

---
+++ mision1/proyecto1/src/com/empresa/gestion/app/Main.java
@@ -0,0 +1,25 @@
+package mision1.proyecto1.src.com.empresa.gestion.app;
+
+import mision1.proyecto1.src.com.empresa.gestion.model.Empleado;
+import mision1.proyecto1.src.com.empresa.gestion.model.Gerente;
+import mision1.proyecto1.src.com.empresa.gestion.service.NominaService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de Gestión de Empleados");
+
+        Empleado emp1 = new Empleado("Juan Pérez", 30, 50000.0);
+        Gerente ger1 = new Gerente("Ana Gómez", 40, 80000.0, "Ventas");
+        emp1.presentarse();
+        ger1.presentarse();
+
+        NominaService nominaService = new NominaService();
+        double salarioEmp1 = nominaService.calcularSalarioAnual(emp1);
+        double salarioGer1 = nominaService.calcularSalarioAnual(ger1);
+        System.out.println("Salario anual de " + emp1.getNombre() + ": " + salarioEmp1);
+        System.out.println("Salario anual de " + ger1.getNombre() + ": " + salarioGer1);
+        
+    }
+    
+}
