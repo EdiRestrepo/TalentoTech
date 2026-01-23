a44850a3c3116dfd9b95bef33a9ba30400e53e17
EdiRestrepo <edisonestival@gmail.com>
2026-01-22 21:20:23 -0500

    feat: implement UserService for fetching user data and update app to display users

--- front/mi-primer-angular/src/app/app.ts
+++ front/mi-primer-angular/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './services/user.service/user.service';
+import { JsonPipe } from '@angular/common'; // Add this import
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'mi-primer-angular';
+
+  users: any[] = [];
+
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users);
+    });
+  }
 }
