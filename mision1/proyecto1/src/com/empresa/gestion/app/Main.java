7466551a5c7966027819ce11b0e7897917872416
EdiRestrepo <edisonestival@gmail.com>
2026-01-22 21:21:53 -0500

    feat: enhance Gerente class with bonus attribute and update Main to display additional employee information

--- mision1/proyecto1/src/com/empresa/gestion/app/Main.java
+++ mision1/proyecto1/src/com/empresa/gestion/app/Main.java
@@ -10,17 +10,24 @@ public class Main {
         System.out.println("Sistema de Gestión de Empleados");
 
         Empleado emp1 = new Empleado("Juan Pérez", 30, true, 'M', 50000.0, 160);
-        Gerente ger1 = new Gerente("Ana Gómez", 40, false, 'F', 80000.0, "Ventas",50);
-        emp1.presentarse();
-        ger1.presentarse();
+        Gerente ger1 = new Gerente("Ana Gómez", 40, false, 'F', 80000.0, "Ventas",50, 10000.0);
+        // emp1.presentarse();
+        // ger1.presentarse();
 
         NominaService nominaService = new NominaService();
         double salarioEmp1 = nominaService.calcularSalarioAnual(emp1);
         double salarioGer1 = nominaService.calcularSalarioAnual(ger1);
+
+        System.out.println("==== EMPLEADOS ====");
         System.out.println("Salario anual de " + emp1.getNombre() + ": " + salarioEmp1);
-        System.out.println("Salario anual de " + ger1.getNombre() + ": " + salarioGer1);
-        System.out.println("\nInformación básica de los empleados:");
+        System.out.println("¿Puede trabajar?"+ (emp1.puedeTrabajar() ? " Sí" : " No"));
         emp1.mostrarInfoBasica();
+        System.out.println("");
+        System.out.println("\n==== GERENTES ====");
+        System.out.println("¿Puede trabajar?"+ (ger1.puedeTrabajar() ? " Sí" : " No"));
+        System.out.println("Salario anual de " + ger1.getNombre() + ": " + salarioGer1);
+        System.out.println("Bono del gerente: " + ger1.getBono());
+        System.out.println("Salario mensual del gerente (incluyendo bono): " + ger1.calcularSalarioMensual());
         ger1.mostrarInfoBasica();
         
     }
--- mision1/proyecto1/src/com/empresa/gestion/model/Gerente.java
+++ mision1/proyecto1/src/com/empresa/gestion/model/Gerente.java
@@ -3,10 +3,19 @@ package mision1.proyecto1.src.com.empresa.gestion.model;
 public class Gerente extends Empleado {
 
     private String departamento;
+    private double bono;
 
-    public Gerente(String nombre, int edad, boolean activo, char genero, double salario, String departamento, int horasTrabajadas) {
+    public Gerente(String nombre, int edad, boolean activo, char genero, double salario, String departamento, int horasTrabajadas, double bono) {
         super(nombre, edad, activo, genero, salario, horasTrabajadas);
         this.departamento = departamento;
+        this.bono = bono;
+    }
+
+    public double getBono() {
+        return bono;
+    }
+    public void setBono(double bono) {
+        this.bono = bono;
     }
 
     public String getDepartamento() {
@@ -21,6 +30,12 @@ public class Gerente extends Empleado {
     public void presentarse() {
         super.presentarse();
         System.out.println("Soy el gerente del departamento de " + departamento + ".");
+    }
+
+    @Override
+    public double calcularSalarioMensual() {
+        double salarioBase = super.calcularSalarioMensual();
+        return salarioBase + bono;
     }
     
 }
